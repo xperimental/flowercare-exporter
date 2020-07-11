49ad847f96237cf9cac539129562f1018f387799
Robert Jacob <xperimental@solidproject.de>
2020-07-11 18:36:38 +0200

    Retry configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -96,6 +96,13 @@ type Config struct {
 	Device          string
 	RefreshDuration time.Duration
 	StaleDuration   time.Duration
+	Retry           RetryConfig
+}
+
+type RetryConfig struct {
+	MinDuration time.Duration
+	MaxDuration time.Duration
+	Factor      float64
 }
 
 func Parse(log logrus.FieldLogger) (Config, error) {
@@ -105,6 +112,11 @@ func Parse(log logrus.FieldLogger) (Config, error) {
 		Device:          "hci0",
 		RefreshDuration: 2 * time.Minute,
 		StaleDuration:   5 * time.Minute,
+		Retry: RetryConfig{
+			MinDuration: 30 * time.Second,
+			MaxDuration: 30 * time.Minute,
+			Factor:      2,
+		},
 	}
 
 	pflag.Var(&result.LogLevel, "log-level", "Minimum log level to show.")
@@ -113,6 +125,9 @@ func Parse(log logrus.FieldLogger) (Config, error) {
 	pflag.StringVarP(&result.Device, "adapter", "i", result.Device, "Bluetooth device to use for communication.")
 	pflag.DurationVarP(&result.RefreshDuration, "refresh-duration", "r", result.RefreshDuration, "Interval used for refreshing data from bluetooth devices.")
 	pflag.DurationVar(&result.StaleDuration, "stale-duration", result.StaleDuration, "Duration after which data is considered stale and is not used for metrics anymore.")
+	pflag.DurationVar(&result.Retry.MinDuration, "retry-min-duration", result.Retry.MinDuration, "Minimum wait time between retries on error.")
+	pflag.DurationVar(&result.Retry.MaxDuration, "retry-max-duration", result.Retry.MaxDuration, "Maximum wait time between retries on error.")
+	pflag.Float64Var(&result.Retry.Factor, "retry-factor", result.Retry.Factor, "Factor used to multiply wait time for subsequent retries.")
 	pflag.Parse()
 
 	if len(result.Sensors) == 0 {
@@ -129,6 +144,18 @@ func Parse(log logrus.FieldLogger) (Config, error) {
 
 	if result.StaleDuration < (2 * result.RefreshDuration) {
 		return result, fmt.Errorf("stale duration needs to be at least %d", 2*result.RefreshDuration)
+	}
+
+	if result.Retry.MinDuration < 30*time.Second {
+		return result, fmt.Errorf("retry time needs to be at least thirty seconds: %s", result.Retry.MinDuration)
+	}
+
+	if result.Retry.MaxDuration < result.Retry.MinDuration {
+		return result, fmt.Errorf("maximum retry time needs to be larger or equal to minimum time: %s > %s", result.Retry.MinDuration, result.Retry.MaxDuration)
+	}
+
+	if result.Retry.Factor < 1 {
+		return result, fmt.Errorf("retry factor needs to be equal or larger than one: %v", result.Retry.Factor)
 	}
 
 	return result, nil
